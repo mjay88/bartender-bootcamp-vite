8d56ff9fa0f79ea1af80115ec061a9b70acba798
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-01-10 22:36:28 -0600

    added custom bootstrap file and custom css file

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/images/Logo.png";
 
 const Header = () => {
 	return (
 		<header>
 			<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
 				<Container>
-					<Navbar.Brand href="/">ProShop</Navbar.Brand>
+					<Navbar.Brand href="/">
+						<img src={logo}></img>
+						Bartender Bootcamp
+					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ms-auto">
