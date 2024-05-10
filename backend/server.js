5e1188cb47d40f7df98acb4860427fb622f8c661
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-05-09 22:27:01 -0500

    working on cors

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,13 @@ app.use(cors());
 app.use(cookieParser());
 
 const corsOptions = {
-	origin: "http://localhost:5000", // replace with your frontend's origin
+	origin: function (origin, callback) {
+		if (origin.startsWith("http://localhost:5000")) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
 	credentials: true, // this allows session cookies to be sent with the request
 };
 
