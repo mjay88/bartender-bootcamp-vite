02b5de41cb435cdb12ba22cec6078c8569b7a0b4
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-05-09 22:14:26 -0500

    change port to 5000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(() => {
 			open: true,
 			// host: "127.0.0.1",
 			host: "localhost",
-			port: 3000,
+			port: 5000,
 			// proxy: {
 			// 	"/api": {
 			// 		// target: "http://127.0.0.1:3000",
